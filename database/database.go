d02d0921abc7c31424f3dc4a7e0a753d3f260cb5
Momchil Ignatov <momchil.ignatov@sap.com>
2021-01-18 21:41:10 +0200

    Db update

--- database/database.go
+++ database/database.go
@@ -9,7 +9,7 @@ var DbConnection *sql.DB
 
 func SetupDatabase() {
 	var err error
-	DbConnection, err = sql.Open("mysql", "root:R580122b@tcp(127.0.0.1:3306)/bloodbankdb")
+	DbConnection, err = sql.Open("mysql", "b2d8489953993a:3f02cb75@tcp(eu-cdbr-west-03.cleardb.net)/heroku_4fa9ce9da159b8c")
 	if err != nil {
 		log.Fatal(err)
 	}
